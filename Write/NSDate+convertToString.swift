5021e1fa131f8d53234ca9c8338449275bdf5f57
Zulwiyoza Putra <zulwiyozaputra@yahoo.com>
2016-10-15 00:43:37 -0700

    New Extension

---
+++ Write/NSDate+convertToString.swift
@@ -0,0 +1,15 @@
+//
+//  NSDate+convertToString.swift
+//  Write
+//
+//  Created by Zulwiyoza Putra on 10/15/16.
+//  Copyright © 2016 Zulwiyoza Putra. All rights reserved.
+//
+
+import Foundation
+
+extension NSDate {
+    func convertToString() -> String {
+        return DateFormatter.localizedString(from: self as Date, dateStyle: DateFormatter.Style.medium, timeStyle: DateFormatter.Style.medium)
+    }
+}
